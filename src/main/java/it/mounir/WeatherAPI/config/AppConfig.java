a3e50f3f7e5300d22eb3f9c7a06d6f3e0ee1ce4e
klil011 <khalilmounir.dev@gmail.com>
2024-10-10 10:02:14 +0200

    added the in-memory service with Redis

--- src/main/java/it/mounir/WeatherAPI/config/AppConfig.java
+++ src/main/java/it/mounir/WeatherAPI/config/AppConfig.java
@@ -1,7 +1,12 @@
 package it.mounir.WeatherAPI.config;
 
+import it.mounir.WeatherAPI.model.WeatherResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
@@ -10,5 +15,19 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public RedisTemplate<String, WeatherResponse> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, WeatherResponse> template = new RedisTemplate<>();
+        template.setConnectionFactory(connectionFactory);
+
+        // Configura il serializer per le chiavi (String)
+        template.setKeySerializer(new StringRedisSerializer());
+
+        // Configura il serializer per i valori (WeatherResponse in formato JSON)
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+
+        return template;
     }
 }
--- src/main/java/it/mounir/WeatherAPI/services/WeatherService.java
+++ src/main/java/it/mounir/WeatherAPI/services/WeatherService.java
@@ -1,11 +1,16 @@
 package it.mounir.WeatherAPI.services;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import it.mounir.WeatherAPI.model.WeatherResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class WeatherService {
@@ -17,13 +22,34 @@ public class WeatherService {
     @Autowired
     private RestTemplate restTemplate;
 
-    public WeatherResponse getWeather(String city) {
-        String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city + "?key=" + apiKey;
+    @Autowired
+    private RedisTemplate<String, WeatherResponse> redisTemplate;
 
+    public WeatherResponse getWeather(String city) {
+        String url = "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/" + city.toLowerCase() + "?key=" + apiKey;
+        String cacheKey = "weather:" + city.toLowerCase();
+
+        /*if data is in the cache */
+        if(redisTemplate.hasKey(cacheKey)) {
+            WeatherResponse inMemoryWeather = redisTemplate.opsForValue().get(cacheKey);
+
+            System.out.println("*** In-Memory ***");
+            return inMemoryWeather;
+        }
+
+        /* if data is not in the cache -> call API */
         try {
-            return restTemplate.getForObject(url, WeatherResponse.class);
+            WeatherResponse apiWeather = restTemplate.getForObject(url, WeatherResponse.class);
+
+            /* save in-memory if data recived from API is not null */
+            if(apiWeather != null){
+                redisTemplate.opsForValue().set(cacheKey, apiWeather, 12, TimeUnit.HOURS);
+            }
+            System.out.println("*** API data ***");
+            return apiWeather;
         } catch(RestClientException e) {
-            System.err.println("Error, impossible to get API request");
+            System.err.println("<Error> impossible to get API request" + e.getMessage());
+
             return null;
         }
 
